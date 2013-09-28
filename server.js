c5d41cc44d36e5285e1d3dc823578163733c7bdb
wkasel <william@jumpfox.com>
2013-09-27 22:12:48 -0700

    Added a server file so static site can be pushed to heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+// =======================================================
+// = Node Server File - Allows us to push this to Heroku =
+// =======================================================
+var http = require('http'),
+    fs = require('fs');
+
+
+fs.readFile('./index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(8000);
+});
