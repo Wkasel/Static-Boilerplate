551c664dd847b2e26c9e3ccd563b5206fa2d26da
wkasel <william@jumpfox.com>
2013-10-08 15:16:55 -0700

    Updated Static Files

--- server.js
+++ server.js
@@ -1,17 +1,50 @@
 // =======================================================
 // = Node Server File - Allows us to push this to Heroku =
 // =======================================================
-var http = require('http'),
-    fs = require('fs');
+//
+// Special Thanks to Azat Mardinof
+// (http://webapplog.com/deployment-of-static-files-to-heroku/)
+// /////////////////////////////////////////////////////////
+var http = require("http"),
+    url = require("url"),
+    path = require("path"),
+    fs = require("fs"),
+    port = process.argv[2] || 8000;
 
+http.createServer(function(request, response) {
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(8000);
-});
+    var uri = url.parse(request.url).pathname
+      , filename = path.join(process.cwd(), uri);
+
+    fs.exists(filename,
+    function(exists) {
+        if (!exists) {
+            response.writeHead(404, {
+                "Content-Type": "text/plain"
+            });
+            response.write("404 Not Found\n");
+            response.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) filename += '/index.html';
+
+        fs.readFile(filename, "binary",
+        function(err, file) {
+            if (err) {
+                response.writeHead(500, {
+                    "Content-Type": "text/plain"
+                });
+                response.write(err + "\n");
+                response.end();
+                return;
+            }
+
+            response.writeHead(200);
+            response.write(file, "binary");
+            response.end();
+        });
+    });
+}).listen(parseInt(port, 10));
+
+console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
